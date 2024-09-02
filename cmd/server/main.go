9af68501f45afd4b9200c2e1475909687629037e
seanomeara96 <seanom96@gmail.com>
2024-09-02 10:42:03 +0100

    update

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -560,7 +560,7 @@ func handleGetFeed(db *sql.DB, render renderPageFunc) htmlHandleFunc {
 		}
 		defer rows.Close()
 
-		top := []GetTopByPostCountResponse{}
+		top := make([]GetTopByPostCountResponse, 0, limit)
 		for rows.Next() {
 			var row GetTopByPostCountResponse
 			if err := rows.Scan(&row.HashtagID, &row.PostCount); err != nil {
@@ -792,6 +792,7 @@ func countHashtagsByPhrase(db *sql.DB, phrase string) (int, error) {
 	if err != nil {
 		return 0, err
 	}
+
 	return count, nil
 }
 
@@ -831,7 +832,7 @@ func getPersonas(limit, offset int) []Persona {
 		{2, "Caroline Mullen ", "", "https://replicate.delivery/yhqm/kqyPgcRDPFZgAxqY5aQqgLtscg6x2zCal815fwTDDQZ15lqJA/out-0.webp"},
 		{3, "Susan Fagan", "", "https://replicate.delivery/yhqm/sxYBygzKehzXHiJk2zdLF4sK3LeZ0jx6ss9qgmbJKcreoXqmA/out-0.webp"},
 		{4, "Clare O'Shea", "", "https://replicate.delivery/yhqm/JqI1VW2t0f3lfkO4T7WfVciDf9BdsuzYcWljhcrFfGjfR9S1E/out-0.webp"},
-		{5, "Aisling O’Reilly", "", "https://replicate.delivery/yhqm/nXCbkLusEIItOJf0UY6JCiXs1oaxrpKLXwV8dkhKMe791LVTA/out-0.webp"},
+		{5, "Aisling O'Reilly", "", "https://replicate.delivery/yhqm/nXCbkLusEIItOJf0UY6JCiXs1oaxrpKLXwV8dkhKMe791LVTA/out-0.webp"},
 		{6, "Stacey Dowling", "", "https://replicate.delivery/yhqm/gMBf17FLmVQnGS9te4MWQJWlE6PdhjZd5Fhl2ycIZj1ftXqmA/out-0.webp"},
 		{7, "Danielle Duffy", "", "https://replicate.delivery/yhqm/rvXUljqA3QqxCtGa64qnmJyn5jh571lvA8Dixinh6LK39S1E/out-0.webp"},
 	}
