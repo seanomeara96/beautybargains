aa55ea5dc95886c5a2e402ada947f3f1c68f38a1
seanomeara96 <seanom96@gmail.com>
2024-11-28 13:09:34 +0000

    imporved coupon page

--- cmd/server/websites.go
+++ cmd/server/websites.go
@@ -2,30 +2,94 @@ package main
 
 import (
 	"fmt"
-	"strings"
 
 	"github.com/gosimple/slug"
 )
 
-// Website struct matching the Websites table
+// Website represents a website entry in the Websites table.
 type Website struct {
-	WebsiteID   int     `json:"website_id"`
-	WebsiteName string  `json:"website_name"`
-	URL         string  `json:"url"`
-	Country     string  `json:"country"`
-	Score       float64 `json:"score"`
-	Screenshot  string  `json:"screenshot"`
-	Path        string  `json:"path"`
+	WebsiteID   int     `json:"website_id"`   // Unique identifier for the website
+	WebsiteName string  `json:"website_name"` // Name of the website
+	URL         string  `json:"url"`          // URL of the website
+	Country     string  `json:"country"`      // Country code of the website
+	Score       float64 `json:"score"`        // Rating or score of the website
+	Icon        string  `json:"icon"`         // Icon file name (if available)
+	Screenshot  string  `json:"screenshot"`   // Screenshot file name
+	Path        string  `json:"path"`         // URL-safe slug or path
 }
 
+// Sample websites data
 var websites = []Website{
-	{1, "BeautyFeatures", "https://www.beautyfeatures.ie", "IE", 10, "www.beautyfeatures.ie_.png", slug.Make("BeautyFeatures")},
-	{2, "LookFantastic", "https://lookfantastic.ie", "IE", 8, "www.lookfantastic.ie_.png", slug.Make("LookFantastic")},
-	{3, "Millies", "https://millies.ie", "IE", 9, "millies.ie_.png", slug.Make("Millies")},
-	{4, "McCauley Pharmacy", "https://www.mccauley.ie/", "IE", 1, "www.mccauley.ie_.png", slug.Make("McCauley Pharmacy")},
-	{5, "Skin Shop", "https://skinshop.ie/", "IE", 5, "skinshop.ie_.png", slug.Make("skin shop")},
-	{6, "Cloud10 Beauty", "https://www.cloud10beauty.com/", "IE", 7, "www.cloud10beauty.com__.png", slug.Make("cloud10 beauty")},
-	{7, "BeautySavers", "https://www.beautysavers.ie/", "IE", 9, "www.beautysavers.ie_.png", slug.Make("beauty savers")},
+	{
+		WebsiteID:   1,
+		WebsiteName: "BeautyFeatures",
+		URL:         "https://www.beautyfeatures.ie",
+		Country:     "IE",
+		Score:       10,
+		Icon:        "https://cdn11.bigcommerce.com/s-63354/product_images/fav_bf.png?t=1712741168",
+		Screenshot:  "www.beautyfeatures.ie_.png",
+		Path:        slug.Make("BeautyFeatures"),
+	},
+	{
+		WebsiteID:   2,
+		WebsiteName: "LookFantastic",
+		URL:         "https://lookfantastic.ie",
+		Country:     "IE",
+		Score:       8,
+		Icon:        "https://www.lookfantastic.ie/ssr-assets/lookfantastic/updated-favicon.png",
+		Screenshot:  "www.lookfantastic.ie_.png",
+		Path:        slug.Make("LookFantastic"),
+	},
+	{
+		WebsiteID:   3,
+		WebsiteName: "Millies",
+		URL:         "https://millies.ie",
+		Country:     "IE",
+		Score:       9,
+		Icon:        "https://millies.ie/cdn/shop/t/18/assets/favicon.png?v=116056058874015240761621352174",
+		Screenshot:  "millies.ie_.png",
+		Path:        slug.Make("Millies"),
+	},
+	{
+		WebsiteID:   4,
+		WebsiteName: "McCauley Pharmacy",
+		URL:         "https://www.mccauley.ie/",
+		Country:     "IE",
+		Score:       1,
+		Icon:        "https://www.mccauley.ie/static/version1731482450/frontend/Uniphar/mccauleys/en_IE/images/favicons/favicon-196x196.png",
+		Screenshot:  "www.mccauley.ie_.png",
+		Path:        slug.Make("McCauley Pharmacy"),
+	},
+	{
+		WebsiteID:   5,
+		WebsiteName: "Skin Shop",
+		URL:         "https://skinshop.ie/",
+		Country:     "IE",
+		Score:       5,
+		Icon:        "https://skinshop.ie/cdn/shop/files/SkinShop_Log0.png",
+		Screenshot:  "skinshop.ie_.png",
+		Path:        slug.Make("skin shop"),
+	},
+	{
+		WebsiteID:   6,
+		WebsiteName: "Cloud10 Beauty",
+		URL:         "https://www.cloud10beauty.com/",
+		Country:     "IE",
+		Score:       7,
+		Icon:        "https://www.cloud10beauty.com/cdn/shop/files/cbjtd-pi83m_32x32_ba7b8fe2-486a-4697-98e6-fa087a4eeb65.webp",
+		Screenshot:  "www.cloud10beauty.com__.png",
+		Path:        slug.Make("cloud10 beauty"),
+	},
+	{
+		WebsiteID:   7,
+		WebsiteName: "BeautySavers",
+		URL:         "https://www.beautysavers.ie/",
+		Country:     "IE",
+		Score:       9,
+		Icon:        "https://www.beautysavers.ie/favicon.ico",
+		Screenshot:  "www.beautysavers.ie_.png",
+		Path:        slug.Make("beauty savers"),
+	},
 }
 
 /* website funcs*/
@@ -38,15 +102,6 @@ func getWebsiteByID(website_id int) (Website, error) {
 		}
 	}
 	return Website{}, fmt.Errorf("no website with id %d", website_id)
-}
-
-func getWebsiteByName(websiteName string) (Website, error) {
-	for _, website := range getWebsites(0, 0) {
-		if strings.EqualFold(website.WebsiteName, websiteName) {
-			return website, nil
-		}
-	}
-	return Website{}, fmt.Errorf("no website with name %s", websiteName)
 }
 
 func getWebsiteByPath(path string) (Website, error) {
