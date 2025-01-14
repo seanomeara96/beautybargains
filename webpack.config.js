7b2f50387c2919d0b8c08e9527c4ec28960f6d8b
seanomeara96 <seanom96@gmail.com>
2025-01-14 08:39:54 +0000

    production js

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-    mode: "development",
+    mode: "production",
     entry: './assets/src/index.js',
     output: {
         filename: 'index.js',
